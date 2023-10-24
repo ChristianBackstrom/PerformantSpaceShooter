f1e849981f8d060e3d48e5e83bb4c8b25dc81b72
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-10-24 10:36:54 +0200

    Moved projectile <=> asteroid collision into a IJobChunk to improve performance

--- Assets/2. Scripts/2.4 Utility/2.4.2 Collision/ProjectileCollisionSystem.cs
+++ Assets/2. Scripts/2.4 Utility/2.4.2 Collision/ProjectileCollisionSystem.cs
@@ -1,43 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using Unity.Burst;
+using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
 [BurstCompile]
-public partial struct BulletCollisionSystem : ISystem
+public partial struct ProjectileCollisionSystem : ISystem
 {
+    private EntityQuery projectileGroup;
+    private EntityQuery asteroidGroup;
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+    }
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var ECB = new EntityCommandBuffer(Allocator.Temp);
+        projectileGroup = state.GetEntityQuery(ComponentType.ReadOnly<LocalTransform>(), ComponentType.ReadOnly<ProjectileTag>());
+        asteroidGroup = state.GetEntityQuery(ComponentType.ReadOnly<LocalTransform>(), ComponentType.ReadOnly<AsteroidTag>());
+        var ecbSingleton = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
         
-        foreach (var projectileAspect in SystemAPI.Query<ProjectileAspect>())
+        var projectileCollisionJob = new ProjectileCollisionJob()
         {
-            float2 projectilePosition = projectileAspect.Position;
+            TransformTypeHandle = state.GetComponentTypeHandle<LocalTransform>(false),
+            EntityTypeHandle = state.GetEntityTypeHandle(),
+            TransformsToTestAgainst = asteroidGroup.ToComponentDataArray<LocalTransform>(Allocator.TempJob),
+            EntitiesToTestAgainst = asteroidGroup.ToEntityArray(Allocator.TempJob),
+            ECB = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged)
+        };
+        
+        state.Dependency = projectileCollisionJob.Schedule(projectileGroup, state.Dependency);
+    }
+}
+
+[BurstCompile]
+public struct ProjectileCollisionJob : IJobChunk
+{
+    [ReadOnly] public ComponentTypeHandle<LocalTransform> TransformTypeHandle;
+    [ReadOnly] public EntityTypeHandle EntityTypeHandle;
+    [ReadOnly] public NativeArray<LocalTransform> TransformsToTestAgainst;
+    [ReadOnly] public NativeArray<Entity> EntitiesToTestAgainst;
+    
+
+    public EntityCommandBuffer ECB;
+
+    [BurstCompile]
+    public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+    {
+        NativeArray<LocalTransform> transformArray = chunk.GetNativeArray(ref TransformTypeHandle);
+        NativeArray<Entity> entities = chunk.GetNativeArray(EntityTypeHandle);
+
+        for (int i = 0; i < transformArray.Length; i++)
+        {
+            LocalTransform position = transformArray[i];
 
             bool hit = false;
-            
-            foreach (var asteroidAspect in SystemAPI.Query<AsteroidFlyAspect>())
+
+            for (int j = 0; j < TransformsToTestAgainst.Length; j++)
             {
                 if (hit) continue;
                 
-                float2 asteroidPosition = asteroidAspect.Position;
-
-                if (MiscMath.GetDistanceSqr(projectilePosition, asteroidPosition) <= 1)
-                {
-                    ECB.DestroyEntity(asteroidAspect.Entity);
-                    ECB.DestroyEntity(projectileAspect.Entity);
-                    hit = true;
-                }
+                LocalTransform position2 = TransformsToTestAgainst[j];
+                
+                if (!(MiscMath.GetDistanceSqr(position.Position.xy, position2.Position.xy) <= 2)) continue;
+                
+                ECB.DestroyEntity(entities[i]);
+                ECB.DestroyEntity(EntitiesToTestAgainst[j]);
+                hit = true;
             }
         }
-        
-        ECB.Playback(state.EntityManager);
-        ECB.Dispose();
     }
 }
+
+
+// foreach (var asteroidAspect in SystemAPI.Query<AsteroidFlyAspect>())
+// {
+//     float2 asteroidPosition = asteroidAspect.Position;
+//
+//     bool hit = false;
+//             
+//     foreach (var projectileAspect in SystemAPI.Query<ProjectileAspect>())
+//     {
+//         if (hit) continue;
+//                 
+//         float2 projectilePosition = projectileAspect.Position;
+//
+//         if (MiscMath.GetDistanceSqr(projectilePosition, asteroidPosition) <= 1)
+//         {
+//             ECB.DestroyEntity(asteroidAspect.Entity);
+//             ECB.DestroyEntity(projectileAspect.Entity);
+//             hit = true;
+//         }
+//     }
+// }
