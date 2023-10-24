a7ff7bf86c537419922347629659be3b6d9a1353
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-10-24 10:36:26 +0200

    Put the lifetime system into a job to increase performance but none was changed

--- Assets/2. Scripts/2.4 Utility/2.4.1 Lifetime/LifetimeAspect.cs
+++ Assets/2. Scripts/2.4 Utility/2.4.1 Lifetime/LifetimeAspect.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public readonly partial struct LifetimeAspect : IAspect
 {
+    public readonly Entity Entity;
+    
     private readonly RefRW<LifetimeComponent> lifetimeTimer;
 
+
+    public float Lifetime => lifetimeTimer.ValueRO.Lifetime;
+    public float Time => lifetimeTimer.ValueRO.Time;
+        
     public void UpdateTime(float DeltaTime)
     {
         lifetimeTimer.ValueRW.Time -= DeltaTime;
-        
-        
     }
 }
--- Assets/2. Scripts/2.4 Utility/2.4.1 Lifetime/LifetimeSystem.cs
+++ Assets/2. Scripts/2.4 Utility/2.4.1 Lifetime/LifetimeSystem.cs
@@ -12,27 +12,82 @@ public partial struct LifetimeSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
-        foreach (var (lifetimeTimer, entity) in SystemAPI.Query<LifetimeComponent>().WithEntityAccess())
-        {
-            float time = lifetimeTimer.Time - SystemAPI.Time.DeltaTime;
+        var ecbSingleton = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
 
-            if (time <= 0)
-            {
-                Debug.Log("Died");
-                ecb.DestroyEntity(entity);
-                continue;
-            }
-            
-            var lifetimer = new LifetimeComponent()
-            {
-                Lifetime = lifetimeTimer.Lifetime,
-                Time = time,
-            };
-            ecb.SetComponent(entity, lifetimer);
-        }
-        
-        ecb.Playback(state.EntityManager);
-        ecb.Dispose();
+        new LifetimeJob()
+        {
+            DeltaTime = SystemAPI.Time.DeltaTime,
+            ECB = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged)
+        }.ScheduleParallel();
+
     }
 }
+
+[BurstCompile]
+public partial struct LifetimeJob : IJobEntity
+{
+    public EntityCommandBuffer ECB;
+    public float DeltaTime;
+    [BurstCompile]
+    private void Execute(LifetimeAspect lifetimeAspect)
+    {
+        float time = lifetimeAspect.Time - DeltaTime;
+
+        if (time <= 0)
+        {
+            Debug.Log("Died");
+            ECB.DestroyEntity(lifetimeAspect.Entity);
+        }
+            
+        var lifetimer = new LifetimeComponent()
+        {
+            Lifetime = lifetimeAspect.Lifetime,
+            Time = time,
+        };
+        
+        ECB.SetComponent(lifetimeAspect.Entity, lifetimer);
+    }
+}
+
+
+// [UpdateInGroup(typeof(LateSimulationSystemGroup))]
+// public partial struct LifetimeSystem : ISystem
+// {
+//     [BurstCompile]
+//     public void OnUpdate(ref SystemState state)
+//     {
+//         var ecbSingleton = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
+//
+//         new LifetimeJob()
+//         {
+//             DeltaTime = SystemAPI.Time.DeltaTime,
+//             ECB = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged)
+//         }.ScheduleParallel();
+//
+//     }
+// }
+//
+// public partial struct LifetimeJob : IJobEntity
+// {
+//     public EntityCommandBuffer ECB;
+//     public float DeltaTime;
+//     private void Execute(LifetimeAspect lifetimeAspect)
+//     {
+//         float time = lifetimeAspect.Time - DeltaTime;
+//
+//         if (time <= 0)
+//         {
+//             Debug.Log("Died");
+//             ECB.DestroyEntity(lifetimeAspect.Entity);
+//         }
+//             
+//         var lifetimer = new LifetimeComponent()
+//         {
+//             Lifetime = lifetimeAspect.Lifetime,
+//             Time = time,
+//         };
+//         
+//         ECB.SetComponent(lifetimeAspect.Entity, lifetimer);
+//     }
+// }
+
