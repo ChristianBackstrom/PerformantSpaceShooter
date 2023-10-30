5a75f3e6ef9581e137f68bda75c97c9376d031c6
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-10-30 13:24:42 +0100

    Player collision is now active

--- Assets/2. Scripts/2.4 Utility/2.4.2 Collision/PlayerCollisionSystem.cs
+++ Assets/2. Scripts/2.4 Utility/2.4.2 Collision/PlayerCollisionSystem.cs
@@ -9,41 +9,34 @@ using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// [BurstCompile]
-// public partial struct PlayerCollisionSystem : ISystem
-// {
-//     [BurstCompile]
-//     public void OnUpdate(ref SystemState state)
-//     {
-//         
-//         var ECB = new EntityCommandBuffer(Allocator.Temp);
-//         
-//         foreach (var (transform, tag, entity) in SystemAPI.Query<LocalTransform, PlayerTag>().WithEntityAccess())
-//         {
-//             float2 projectilePosition = transform.Position.xy;
-//
-//             bool hit = false;
-//             
-//             foreach (var asteroidAspect in SystemAPI.Query<AsteroidFlyAspect>())
-//             {
-//                 if (hit) continue;
-//                 
-//                 float2 asteroidPosition = asteroidAspect.Position;
-//
-//                 if (MiscMath.GetDistanceSqr(projectilePosition, asteroidPosition) <= 1)
-//                 {
-//                     CleanAndRestartECS();
-//                     hit = true;
-//                 }
-//             }
-//         }
-//         
-//         ECB.Playback(state.EntityManager);
-//         ECB.Dispose();
-//     }
-//     
-//     private void CleanAndRestartECS()
-//     {
-//         SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
-//     }
-// }
+[BurstCompile]
+public partial struct PlayerCollisionSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var ECB = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>()
+            .CreateCommandBuffer(state.WorldUnmanaged);
+        foreach (var (transform, tag, entity) in SystemAPI.Query<LocalTransform, PlayerTag>().WithEntityAccess())
+        {
+            float2 projectilePosition = transform.Position.xy;
+
+            bool hit = false;
+            
+            foreach (var (asteroidTransform, asteroidTag, asteroidEntity) in SystemAPI.Query<LocalTransform, AsteroidTag>().WithEntityAccess())
+            {
+                if (hit) continue;
+                
+                float2 asteroidPosition = asteroidTransform.Position.xy;
+
+                if (MiscMath.GetDistanceSqr(projectilePosition, asteroidPosition) <= 1)
+                {
+                    hit = true;
+                    
+                    ECB.DestroyEntity(entity);
+                    ECB.DestroyEntity(asteroidEntity);
+                }
+            }
+        }
+    }
+}
